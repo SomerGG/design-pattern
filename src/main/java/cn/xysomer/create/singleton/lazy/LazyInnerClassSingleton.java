b8874626b56439009a44fa9e6e6a37ce49f5fe5f
Somer <xuyangsomer@outlook.com>
2020-02-13 20:44:01 +0800

    修改：单例模式

--- src/main/java/cn/xysomer/create/singleton/lazy/LazyInnerClassSingleton.java
+++ src/main/java/cn/xysomer/create/singleton/lazy/LazyInnerClassSingleton.java
@@ -5,10 +5,8 @@ package cn.xysomer.create.singleton.lazy;
  *
  */
 //没有使用 synchronized，性能最优
-//根据类加载的顺序，当加载主类的时候，先初始化内部类，执行相应逻辑
 public class LazyInnerClassSingleton {
 
-    //如果没使用的话，内部类是不加载的
     private LazyInnerClassSingleton(){
         if (null != LazyHolder.LAZY) {//防止反射攻击
             throw new RuntimeException("不允许构建多个实例");
@@ -20,11 +18,11 @@ public class LazyInnerClassSingleton {
     //JVM 底层执行逻辑，完美避开了线程安全问题
     //static 是为了使单例的空间共享
     //final 是保证该方法不会被重写、重载
-    public static LazyInnerClassSingleton getInstance() {
+    public static final LazyInnerClassSingleton getInstance() {
         return LazyHolder.LAZY;
     }
 
-    //默认不加载
+    //如果没使用的话，内部类是不加载的，当调用 getInstance 时才会进行加载该静态内部类
     private static class LazyHolder {
         private static final LazyInnerClassSingleton LAZY = new LazyInnerClassSingleton();
     }
